5c81c90d3ab802dd34b988058f5eab88b9a6a991
mansi <mansijajadiya@gmail.com>
2026-09-09 17:09:58 +0530

    added some comments

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import App, { App2 } from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <App2 />
   </StrictMode>,
 )
